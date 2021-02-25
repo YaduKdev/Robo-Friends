5098b82a4bd5c401e6ac9131aaad6afeea7c3053
YaduKdev <YaduK.developer@gmail.com>
2021-02-26 00:48:30 +0530

    adding react files

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ import "./Header.css";
 const Header = ({searchChange}) => {
     return (
         <div>
-            <h1 className="f1">RoboFriends</h1>
+            <h1 className="f1">RoboFriendzz</h1>
             <SearchBox searchChangePass={searchChange} />
         </div>
     );
